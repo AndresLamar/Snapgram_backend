b4dc2c00ebf93a84c024b70eb0dca58968534ad0
Andres Lamar <andresisfelipe20@gmail.com>
2024-05-17 17:41:18 -0500

    add accepted origin to cors

--- config.js
+++ config.js
@@ -19,3 +19,5 @@ export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
 
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
+
+export const FRONTEND_URL = process.env.FRONTEND_URL
