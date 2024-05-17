29e7a0b76db8b2b05626951ff76604c3a90a4361
Andres Lamar <andresisfelipe20@gmail.com>
2024-05-17 17:59:04 -0500

    FIX: SSL rejectUnauthorized

--- db/postgresql.js
+++ db/postgresql.js
@@ -2,8 +2,10 @@ import pg from 'pg'
 import { DB_CONNSTRING } from "../config.js";
 
 const pool = new pg.Pool({
+    ssl: { rejectUnauthorized: true },
     connectionString: DB_CONNSTRING,
     ssl: true
+
 })
 
 
