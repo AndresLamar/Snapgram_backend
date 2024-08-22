d1a6207a98b18acb4ff5472b70708889da674eab
Andres Lamar <andresisfelipe20@gmail.com>
2024-08-22 13:37:16 -0500

    Change DB to Supabase

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-import express, { json } from 'express' // require -> commonJS
-import { createUserRouter } from './routes/users.js'
-import { createLoginRouter } from './routes/login.js'
-import { createPostRouter } from './routes/posts.js'
-import { createUploadRouter } from './routes/upload.js'
+import express, { json } from "express"; // require -> commonJS
+import { createUserRouter } from "./routes/users.js";
+import { createLoginRouter } from "./routes/login.js";
+import { createPostRouter } from "./routes/posts.js";
+import { createUploadRouter } from "./routes/upload.js";
 // import { corsMiddleware } from './middlewares/cors.js'
-import cors from 'cors'
+import cors from "cors";
 import fileUpload from "express-fileupload";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234;
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-dotenv.config()
-export const createApp = ({ userModel, loginModel, postModel, uploadModel }) => {
-  const app = express()
-  app.use(json())
-  app.use(cors())
+dotenv.config();
+export const createApp = ({
+  userModel,
+  loginModel,
+  postModel,
+  uploadModel,
+}) => {
+  const app = express();
+  app.use(json());
+  app.use(cors());
 
-  app.disable('x-powered-by')
-  app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: './uploads',
-    limits: { fileSize: 10 * 1024 * 1024 },  
-  }))
+  app.disable("x-powered-by");
+  app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "./uploads",
+      limits: { fileSize: 10 * 1024 * 1024 },
+    })
+  );
 
-  app.use('/api/users', createUserRouter({ userModel }))
-  app.use('/api/login', createLoginRouter({ loginModel }))
-  app.use('/api/posts', createPostRouter({ postModel }))
-  app.use('/api/upload', createUploadRouter({ uploadModel }))
+  app.use("/api/users", createUserRouter({ userModel }));
+  app.use("/api/login", createLoginRouter({ loginModel }));
+  app.use("/api/posts", createPostRouter({ postModel }));
+  app.use("/api/upload", createUploadRouter({ uploadModel }));
 
   app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`)
-  })
-}
+    console.log(`server listening on port ${PORT}`);
+  });
+};
--- db/postgresql.js
+++ db/postgresql.js
@@ -1,12 +1,10 @@
-import pg from 'pg'
+import pg from "pg";
 import { DB_CONNSTRING } from "../config.js";
 
 const pool = new pg.Pool({
-    ssl: { rejectUnauthorized: true },
-    connectionString: DB_CONNSTRING,
-    ssl: true
+  // ssl: { rejectUnauthorized: true },
+  connectionString: DB_CONNSTRING,
+  ssl: true,
+});
 
-})
-
-
-export default pool
+export default pool;
